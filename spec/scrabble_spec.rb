af799c424182957b89db87869c5b56a61efcb993
Jonathan Ly <jonathanlyhv@gmail.com>
2017-07-12 15:44:29 +1000

    Added convert_to_letter_values method

--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -1,13 +1,21 @@
 require 'scrabble'
 
-describe Scrabble do
+RSpec.describe 'Scrabble App' do
 
   before(:all) do
     @scrabble = Scrabble.new
   end
 
-  it "creates an instance of the Scrabble class" do
+  it "create an instance of the Scrabble class" do
     expect(@scrabble).to be_an_instance_of(Scrabble)
   end
+
+  it "converts a string to an array of corresponding character values" do
+    hello = @scrabble.convert_to_letter_values("Hello")
+    expect(hello).to be_an_instance_of(Array)
+    expect(hello).to eq([4, 1, 1, 1, 1])
+  end
+
+
 
 end
