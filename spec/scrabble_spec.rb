41ba95fb63900d6165aeb1e5a40db58208cd4dc6
Jonathan Ly <jonathanlyhv@gmail.com>
2017-07-13 00:16:48 +1000

    Refactored unit tests to follow convention and be more descriptive

--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -10,58 +10,70 @@ RSpec.describe Scrabble do
     expect(@scrabble).to be_an_instance_of Scrabble
   end
 
-  it "converts a string to an array of corresponding character values" do
-    hello = @scrabble.convert_to_letter_values("Hello")
-    expect(hello).to be_an_instance_of Array
-    expect(hello).to eq [4, 1, 1, 1, 1]
-  end
-
-  describe "score method" do
-    it "returns 0 if nil is passed as a parameter" do
-      expect(@scrabble.score(nil)).to eq 0
-    end
-
-    it "returns 0 an empty string is passed as a parameter" do
-      expect(@scrabble.score("")).to eq 0
-    end
-
-    it "returns a number when a string is passed as a parameter" do
-      expect(@scrabble.score("example")).to be_an_instance_of Fixnum
-    end
-
-    it "returns the correct score for a word" do
-      expect(@scrabble.score("example")).to eq 18
-      expect(@scrabble.score("unicorn")).to eq 9
-      expect(@scrabble.score("developer")).to eq 15
-    end
-
-    it "is insensitive to character case" do
-      expect(@scrabble.score("example")).to eq 18
-      expect(@scrabble.score("eXaMplE")).to eq 18
+  describe "#convert_to_letter_values" do
+    it "converts a string to an array of corresponding letter values" do
+      hello = @scrabble.convert_to_letter_values("Hello")
+      expect(hello).to be_an_instance_of Array
+      expect(hello).to eq [4, 1, 1, 1, 1]
     end
   end
 
-  it "calculates double word score correctly" do
-    expect(@scrabble.double_word("example")).to eq 36
-    expect(@scrabble.double_word("unicorn")).to eq 18
-    expect(@scrabble.double_word("developer")).to eq 30
+
+  describe "#score" do
+    context "on valid argument input" do
+      it "returns a number" do
+        expect(@scrabble.score("example")).to be_an_instance_of Fixnum
+      end
+
+      it "returns correct score for a word" do
+        expect(@scrabble.score("example")).to eq 18
+      end
+
+      it "is insensitive to character case" do
+        expect(@scrabble.score("eXaMplE")).to eq 18
+      end
+
+      it "can calculate two words" do
+        expect(@scrabble.score("cat videos")).to eq 15
+      end
+    end
+
+    context "on invalid argument input" do
+      it "returns 0 if nil" do
+        expect(@scrabble.score(nil)).to eq 0
+      end
+
+      it "returns 0 if string is empty" do
+        expect(@scrabble.score("")).to eq 0
+      end
+
+      it "returns 0 if characters are invalid" do
+        expect(@scrabble.score("!@$%^")).to eq 0
+      end
+    end
   end
 
-  it "calculates triple word score correctly" do
-    expect(@scrabble.triple_word("example")).to eq 54
-    expect(@scrabble.triple_word("unicorn")).to eq 27
-    expect(@scrabble.triple_word("developer")).to eq 45
+  describe "#double_word" do
+    it "calculates double word score correctly" do
+      expect(@scrabble.double_word("example")).to eq 36
+    end
   end
 
-  it "calculates double letter score correctly" do
-    expect(@scrabble.double_letter("example", "x")).to eq 26
-    expect(@scrabble.double_letter("unicorn", "c")).to eq 12
-    expect(@scrabble.double_letter("developer", "e")).to eq 16
+  describe "#triple_word" do
+    it "calculates triple word score correctly" do
+      expect(@scrabble.triple_word("example")).to eq 54
+    end
   end
 
-  it "calculates triple letter score correctly" do
-    expect(@scrabble.triple_letter("example", "x")).to eq 34
-    expect(@scrabble.triple_letter("unicorn", "c")).to eq 15
-    expect(@scrabble.triple_letter("developer", "e")).to eq 17
+  describe "#double_letter" do
+    it "calculates double letter score correctly" do
+      expect(@scrabble.double_letter("example", "x")).to eq 26
+    end
+  end
+
+  describe "#triple_letter" do
+    it "calculates triple letter score correctly" do
+      expect(@scrabble.triple_letter("example", "x")).to eq 34
+    end
   end
 end
